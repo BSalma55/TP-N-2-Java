d4f830773e2ebe4c749d54c8151b0fd05a5b79ff
Salma BALMIR <168284540+BSalma55@users.noreply.github.com>
2026-01-11 15:35:20 +0100

    Refactor Product class with updated setters

--- Exercice 1 /Liste/Product.java
+++ Exercice 1 /Liste/Product.java
@@ -1,4 +1,5 @@
 public class Product {
+    // Attributs
     private long id;
     private String name;
     private double price;
@@ -10,9 +11,13 @@ public class Product {
         this.price = price;
     }
 
-    // Getters et setters
+    // Getters et Setters
     public long getId() {
         return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -31,7 +36,7 @@ public class Product {
         this.price = price;
     }
 
-    // Affichage
+    // afficher un produit
     @Override
     public String toString() {
         return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
